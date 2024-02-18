1901b3464b3cd3ac56f77edd1dac53a022ae8695
AlexisJeria <alexisjebe@gmail.com>
2024-02-18 20:59:19 -0300

    noveno Commit, Se agrego estilos y funcionalidades js, proyecto terminado

---
+++ assets/js/jquery.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function () {
+    
+    $("#enviarMensaje").click(function(otro){
+        alert("El mensae fue enviado correctamente");
+    });
+
+   
+
+    $(".imgCard").click(function () {
+        $(".bodyCard").toggle();
+    });
+
+});
+
+
